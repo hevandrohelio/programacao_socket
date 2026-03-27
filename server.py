8f307e4701e7176091da97a22298ca5893426f0b
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2026-03-27 18:54:10 -0300

    Update server.py

--- server.py
+++ server.py
@@ -15,18 +15,50 @@ server.listen()
 
 print("Servidor disponível na porta 24000 e escutando...")
 users={}
-while 1:
-    conn, addr = server.accept() # conn = socket do cliente, addr = ip do cliente
-    print("Conectado por: ", addr)
-    # função para lidar com o cliente
-    def handle_client(conn):
-        while True:
+
+# função para lidar com o cliente
+def handle_client(conn, name):
+    while True:
+        try:
             data = conn.recv(BUFFER_SIZE)
-            name = conn.recv(BUFFER_SIZE).decode().strip()
 
             if not data:
                 break
-            print(data.decode())
 
-    thread = threading.Thread(target=handle_client, args=(conn,))
-    thread.start
+            message = data.decode().strip()
+            print(f"{name}: {message}")
+
+            # broadcast
+            for user, client_conn in users.items():
+                try:
+                    client_conn.send(f"{name}: {message}".encode())
+                except:
+                    pass
+
+        except:
+            break
+
+    # cliente saiu
+    print(f"{name} saiu")
+    del users[name]
+    conn.close()
+
+while 1:
+    conn, addr = server.accept() # conn = socket do cliente, addr = ip do cliente
+    print("Conectado por: ", addr)
+
+    name = conn.recv(BUFFER_SIZE).decode().strip()
+    users[name] = conn
+    print(f"{name} entrou no chat.")
+
+    for client_conn in users.values():
+        try:
+            client_conn.send(f"{name} entrou no chat".encode())
+        except:
+            pass
+
+    
+    thread = threading.Thread(target=handle_client, args=(conn,name))
+    thread.start()
+
+server.close()
